87d3379827364a6228bdf942f3b237554b3b93fc
Titusrono <tituskrono04@gmail.com>
2023-03-16 09:16:48 -0700

    repo 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+*main - display the main function
+*
+* Return: it always return 0
+*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
