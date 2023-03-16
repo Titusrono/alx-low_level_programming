e3a5a722b73ab344dcf1ec6d3c12f408e87a3338
Titusrono <tituskrono04@gmail.com>
2023-03-16 09:22:30 -0700

    repository 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - show that it is main function
+ *
+ *Return: always it returns 0
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+
+}
