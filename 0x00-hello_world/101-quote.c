edc4b535677d5af821fb88ebd56d35ef645e6de4
Titusrono <tituskrono04@gmail.com>
2023-03-17 04:17:41 -0700

    welcome

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+*main - A c program that print a line to the standard output of the main function
+*Return: 1 (success)
+*
+*/
+int main(void)
+{
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n" stdout);
+return (1);
+
+}
